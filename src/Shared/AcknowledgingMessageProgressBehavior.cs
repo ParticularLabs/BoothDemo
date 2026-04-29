b32997fef4cad963af3f59f429aa8a08e820be18
SzymonPobiega <szymon.pobiega@gmail.com>
2026-04-29 08:49:09 +0200

    Fix inspection

--- src/Shared/AcknowledgingMessageProgressBehavior.cs
+++ src/Shared/AcknowledgingMessageProgressBehavior.cs
@@ -23,7 +23,9 @@ public class AcknowledgingMessageProgressBehavior(UserInterface userInterface) :
 
             await userInterface.SendEvent(new ProcessingCompleted(context.Message.MessageId));
         }
+#pragma warning disable PS0019
         catch (Exception e)
+#pragma warning restore PS0019
         {
             await userInterface.SendEvent(new ProcessingFailed(context.Message.MessageId, e.Message));
             throw;
